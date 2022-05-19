2e9e00b2194b7be13d97e5d7d3e5484b4b1fe33f
Lkqas <nazigenuis@proton.me>
2022-05-19 13:59:08 +0200

    stdin fix

--- install.bash
+++ install.bash
@@ -50,9 +50,9 @@ if  [[ $(uname -o) = 'Android' ]]; then
     if echo $PREFIX | grep -o "com.termux"; then
         if [[ $EUID -eq 0 ]]; then
             androidreq="telethon toml rich" 
-            echo -n "Installing packcages..."; pkg upgrade -y && pkg update -y && pkg install -y git python > /dev/null; echo " All packages has been installed!";
-            echo -n "Installing pip packages..."; pip3 install -U $androidreq  > /dev/null; echo " All pip packages has been installed!";
-            echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet > /dev/null; echo "Botnet successfully installed!";
+            echo -n "Installing packcages..."; pkg upgrade -y && pkg update -y && pkg install -y git python &>/dev/null; echo " All packages has been installed!";
+            echo -n "Installing pip packages..."; pip3 install -U $androidreq  &>/dev/null; echo " All pip packages has been installed!";
+            echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null; echo "Botnet successfully installed!";
 
             cd ~/telegram-raid-botnet
             python3 main.py
@@ -62,9 +62,9 @@ if  [[ $(uname -o) = 'Android' ]]; then
             exit 1;
     fi
 if cat /etc/*release | grep ^NAME | grep CentOS || cat /etc/*release | grep ^NAME | grep Red || cat /etc/*release | grep ^NAME | grep Fedora; then 
-    echo -n "Installing Packcages...";  yum install python39 && yum install python39-pip && yum install git > /dev/null; echo " All packages has been installed!"; 
-    echo -n "Installing Pip packages..."; pip3.9 install -U $req > /dev/null; echo "Pip packages had been installed!"; 
-    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet > /dev/null; echo "Botnet successfully installed!";
+    echo -n "Installing Packcages...";  yum install python39 && yum install python39-pip && yum install git &>/dev/null; echo " All packages has been installed!"; 
+    echo -n "Installing Pip packages..."; pip3.9 install -U $req &>/dev/null; echo "Pip packages had been installed!"; 
+    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null; echo "Botnet successfully installed!";
 
     sleep 5
     cd ~/telegram-raid-botnet
@@ -72,27 +72,27 @@ if cat /etc/*release | grep ^NAME | grep CentOS || cat /etc/*release | grep ^NAM
 fi
 if [[ "$OSTYPE" =~ ^WSL2 ]]; then
     echo " Warning you are using WSL botnet may work with errors "
-    echo -n "Installing Packcages...";  apt install -y python ffmpeg youtube-dl git > /dev/null; echo " All packages has been installed!"; 
-    echo -n "Installing Pip packages..."; pip3 install -U $req > /dev/null; echo "Pip packages had been installed!"; 
-    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet > /dev/null; echo "Botnet successfully installed!";
+    echo -n "Installing Packcages...";  apt install -y python ffmpeg youtube-dl git &>/dev/null; echo " All packages has been installed!"; 
+    echo -n "Installing Pip packages..."; pip3 install -U $req &>/dev/null; echo "Pip packages had been installed!"; 
+    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null; echo "Botnet successfully installed!";
     sleep 5
 
     cd ~/telegram-raid-botnet
     python3 main.py
 fi
 if cat /etc/*release | grep ^NAME | grep "Arch Linux" || cat /etc/*release | grep ^NAME | grep "Artix Linux" || cat /etc/*release | grep ^NAME | grep Antix || cat /etc/*release | grep ^NAME | grep Manjaro || cat /etc/*release | grep ^NAME | grep Parabola; then
-    echo -n "Installing Packcages...";  sudo pacman -S ffmpeg git python-pip youtube-dl python > /dev/null; echo " All packages has been installed!"; 
-    echo -n "Installing Pip packages..."; pip install -U $req && pip3 install git+https://github.com/pytgcalls/pytgcalls -U > /dev/null; echo "Pip packages had been installed!"; 
-    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet > /dev/null; echo "Botnet successfully installed!";
+    echo -n "Installing Packcages...";  sudo pacman -S ffmpeg git python-pip youtube-dl python &>/dev/null; echo " All packages has been installed!"; 
+    echo -n "Installing Pip packages..."; pip install -U $req && pip3 install git+https://github.com/pytgcalls/pytgcalls -U &>/dev/null; echo "Pip packages had been installed!"; 
+    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null; echo "Botnet successfully installed!";
     sleep 5
 
     cd ~/telegram-raid-botnet
     python main.py    
 fi
 if  cat /etc/*release | grep ^NAME | grep Ubuntu || cat /etc/*release | grep ^NAME | grep Debian || cat /etc/*release | grep ^NAME | grep Mint || cat /etc/*release | grep ^NAME | grep Mint; then
-    echo -n "Installing Packcages...";  apt update -y && apt install -y ffmpeg git software-properties-common curl && add-apt-repository ppa:deadsnakes/ppa -y && apt install -y python3.10 python3.10-distutils && curl https://bootstrap.pypa.io/get-pip.py | python3.10 && curl -sL https://deb.nodesource.com/setup_17.x | sudo bash - && sudo apt-get install -y nodejs > /dev/null; echo " All packages has been installed!"; 
-    echo -n "Installing Pip packages..."; python3.10 -m pip install -U $req && python3.10 -m pip install git+https://github.com/pytgcalls/pytgcalls -U > /dev/null; echo "Pip packages had been installed!"; 
-    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet > /dev/null; echo "Botnet successfully installed!";        
+    echo -n "Installing Packcages...";  apt update -y && apt install -y ffmpeg git software-properties-common curl && add-apt-repository ppa:deadsnakes/ppa -y && apt install -y python3.10 python3.10-distutils && curl https://bootstrap.pypa.io/get-pip.py | python3.10 && curl -sL https://deb.nodesource.com/setup_17.x | sudo bash - && sudo apt-get install -y nodejs &>/dev/null; echo " All packages has been installed!"; 
+    echo -n "Installing Pip packages..."; python3.10 -m pip install -U $req && python3.10 -m pip install git+https://github.com/pytgcalls/pytgcalls -U &>/dev/null; echo "Pip packages had been installed!"; 
+    echo -n "Starting installing botnet..." git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null; echo "Botnet successfully installed!";        
     sleep 5 
 
     cd telegram-raid-botnet

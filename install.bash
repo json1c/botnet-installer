118f023578016e64631cef4798e38b95967569cb
Lkqas <nazigenuis@proton.me>
2022-07-27 18:42:51 +0200

    Refactored code + add new checks + refectored old checks

--- install.bash
+++ install.bash
@@ -30,28 +30,36 @@ error() {
 last_commit() {
 if [ -d .git ]; then
   COMMIT=$(git show-branch --no-name HEAD)
-  success "[*] Last commit is: $COMMIT";
+  success "[ ✔ ] Last commit is: $COMMIT";
 else
-  warning "[*] .git dir not detected"
+  warning "[ * ] .git dir not detected"
+fi
+}
+
+locale_detect() {
+if locale -a | grep ^ru_RU &>/dev/null; then
+    success "[ * ] You are have ru_RU locale, botnet may work"
+else
+    error "[ !! ] YOUR SYSTEM DOESN'T HAVE A ru_RU LOCALE"
+    error "[ !! ] PLEASE INSTALL ru_RU LOCALE, CAUSE BOTNET MAY NOT WORK WITH OTHERS LOCALES"
 fi
 }
 
 os_print() {
     if command -v termux-setup-storage &>/dev/null; then
-        warning "[*] Your system is: Termux"
+        warning "[ ~ ] Your system is: Termux"
         termux_install
     else
         DISTRO=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
-        warning "[*] Your system is: $DISTRO"
+        warning "[ ~ ] Your system is: $DISTRO"
     fi
 }
 
 script_exit_msg() {
-    error "[!] An error accurated"
-    error "[*] Try to install botnet from instruction | https://teletype.in/@lkqas/telegram-raid-botnet"
-    clear 
+    error "[ !! ] An error accurated"
+    error "[ * ] Try to install botnet from instruction | https://teletype.in/@lkqas/telegram-raid-botnet"
     time_sleep
-    success "[*] Exiting from installer..."
+    success "[ * ] Exiting from installer..."
 }
 
 time_sleep() {
@@ -59,10 +67,10 @@ time_sleep() {
 }
 
 if command -v termux-setup-storage &>/dev/null; then
-    success "[*] Termux detected..." && success "[*] Root not required..."
+    success "[ ✔ ] Termux detected" && success "[ ~ ] Root not required"
 elif [ "$EUID" -ne 0 ] && [[ "$OSTYPE" == "linux-gnu"* ]]; then 
-    echo "[*] Linux detected..."
-    error "[!] Run script from root user!"
+    echo "[ * ] Linux detected..."
+    error "[ !! ] Run script from root user!"
     exit 1;
 fi
 
@@ -71,36 +79,44 @@ if [ "$dir_detected" = "yes" ]; then
 fi
 
 dir_not_found() {
-    warning '[Warning]: Script not detected botnet into your system !'
-    echo '[?] Install botnet to your home dir? (y/n) : '
+    warning '[ ! ]: Script not detected botnet into your system !'
+    echo '[ ? ] Install botnet to your home dir? (y/n) : '
     old_stty_cfg=$(stty -g)
     stty raw -echo
     answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
     stty "$old_stty_cfg"
         if echo "$answer" | grep -iq "^y" ;then
-            echo "[*] Installing botnet..."
+            echo "[ * ] Installing botnet..."
             git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null 
-            success "[*] Installed!"
-            success "[*] Please, restart script"
+            success "[ ✔ ] Installed!"
+            success "[ ~ ] Please, restart script"
             exit 1;
         else 
-            error "[*] User aborted installation"
+            error "[ !! ] User aborted installation"
             exit 1;
+fi
+}
+
+docker_check () {
+if [ -f /.dockerenv ]; then
+    error "[ !! ] You are running auto-install script from docker, the auto-installer cannot function properly under the docker"
+else
+    success "[ ✔ ] Docker not detected...";
 fi
 }
 
 if [ -d "$HOME/telegram-raid-botnet" ]; then
     {
         cd "$HOME/telegram-raid-botnet"
-        warning "[*] Botnet dir detected..."
+        warning "[ ✔ ] Botnet dir detected..."
         time_sleep
         mkdir "$HOME/old-botnet"
         mv "$HOME/telegram-raid-botnet" "$HOME/old-botnet" || error "[*] Dir ~/old-botnet is exist!"
-        warning "[*] Previous botnet was moved to ~/old-botnet"
+        warning "[ ~ ] Previous botnet was moved to ~/old-botnet"
     }
     dir_detected="yes"
 else
-    success "[*] Botnet dir not detected"
+    success "[ ✔ ] Botnet dir not detected"
     fi
 
 dir_detection() {
@@ -108,12 +124,12 @@ if [ -d "$HOME/telegram-raid-botnet" ]; then
     dir_detected="yes"
     {   
         cd "$HOME/telegram-raid-botnet"
-        warning "[*] Botnet dir detected..."
+        warning "[ ✔ ] Botnet dir detected..."
         git pull &>/dev/null || error "[~] An error accurated when user tryed to update botnet"; choice_msg;
-        warning "[*] Succefully updated!"
+        warning "[ ✔ ] Succefully updated!"
     }
         else
-            success "[*] Botnet dir not detected"  
+            success "[ ✔ ] Botnet dir not detected"  
             dir_not_found
     fi
 }
@@ -121,20 +137,20 @@ if [ -d "$HOME/telegram-raid-botnet" ]; then
 termux_install() {
     if [[ $(uname -o) = 'Android' ]]; then
         if echo "$PREFIX" | grep -o "com.termux" &>/dev/null; then
-            echo "[*] Installing packcages..."
+            echo "[ * ] Installing packcages..."
             pkg update -y &>/dev/null && pkg upgrade -y &>/dev/null && pkg install -y git python &>/dev/null 
-            success "[*] All packages has been installed!"
-            echo "[*] Clonning botnet from git..."
+            success "[ ✔ ] All packages has been installed!"
+            echo "[ * ] Clonning botnet from git..."
             git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null 
-            success "[*] Botnet clonned into your system!"
-            echo "[*] Entering to botnet directory..."
+            success "[ ✔ ] Botnet clonned into your system!"
+            echo "[ * ] Entering to botnet directory..."
             cd ~/telegram-raid-botnet
-            success "[*] All ok"
-            echo "[*] Installing pip packages..."
+            success "[ ✔ ] All ok"
+            echo "[ * ] Installing pip packages..."
             pip3 install -r requirements.txt  &>/dev/null
-            success "[*] All pip packages has been installed!"
+            success "[ ✔ ] All pip packages has been installed!"
 
-            success "[*] Starting botnet..." && python3 main.py
+            success "[ ~ ] Starting botnet..." && python3 main.py
         fi
     fi
 }
@@ -147,18 +163,18 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty "$old_stty_cfg"
 if echo "$answer" | grep -iq "^y" ;then
-    echo "[*] Installing important packages..."
+    echo "[ * ] Installing important packages..."
     sudo apt install software-properties-common curl git -y &>/dev/null 
-    success "[*] Installed!"
-    echo "[*] Installing repo for python 3.10..."
+    success "[ ✔ ] Installed!"
+    echo "[ * ] Installing repo for python 3.10..."
     sudo add-apt-repository ppa:deadsnakes/ppa -y &>/dev/null
-    success "[*] Installed!"
-    echo "[*] Installing python 3.10"
+    success "[ ✔ ] Installed!"
+    echo "[ ~ ] Installing python 3.10"
     sudo apt install -y python3.10 python3.10-distutils &>/dev/null && curl -s https://bootstrap.pypa.io/get-pip.py | python3.10 &>/dev/null
-    echo "[*] Installed!"
+    echo "[ ✔ ] Installed!"
 else
     clear
-    warning '[*] User Aborted python 3.10 Installation.'
+    warning '[ ! ] User Aborted python 3.10 Installation.'
     exit 1;
 fi
 }
@@ -172,7 +188,7 @@ do
         "Install BoTneT") break ;;
         "Update BoTneT") dir_detection ;;
         "exit") exit 1 ;;
-        *) error "[*] Enter valid option" ;;
+        *) error "[ ! ] Enter valid option" ;;
     esac
 done
 }
@@ -188,76 +204,62 @@ cat << "EOF"
 EOF
 choice_msg
 os_print
-warning "[*] Botnet now installing into your system..." 
-warning "[*] Wait 5-7 minutes..." 
+docker_check
+locale_detect
+warning "[ * ] Botnet now installing into your system..." 
+warning "[ * ] Wait 5-7 minutes..." 
 time_sleep
 
+####################### DISTROS CHECK AND INSTALL #####################
 
-###################   DEBIAN BASE DISTROS  ###############################################
-
-if [ -f '/etc/debian_version' ]; then
-        ubuntu_python
-        echo "[*] Clonning botnet from git..."
+if cat /etc/*release | grep ^ID_LIKE | grep debian; then
+        ubuntu_python;
+        echo "[ * ] Clonning botnet from git..."
         git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null
-        success "[*] Botnet clonned into your system!"
-        echo "[*] Entering to botnet directory..."
+        success "[ ✔ ] Botnet clonned into your system!"
+        echo "[ * ] Entering to botnet directory..."
         cd ~/telegram-raid-botnet &>/dev/null
-        success "[*] All ok"
-        echo "[*] Installing packcages..."
-        sudo apt update  &>/dev/null && sudo apt install -y ffmpeg git &>/dev/null && curl -sL https://deb.nodesource.com/setup_17.x | sudo bash - &>/dev/null && sudo apt-get install -y nodejs &>/dev/null
-        success "[*] All packages has been installed!"
-        echo "[*] Installing pip packages..."
+        success "[ ✔ ] All ok"
+        echo "[ * ] Installing packcages..."
+        sudo apt update  &>/dev/null && sudo apt install -y ffmpeg youtube-dl &>/dev/null && curl -sL https://deb.nodesource.com/setup_17.x | sudo bash - &>/dev/null && sudo apt-get install -y nodejs &>/dev/null
+        success "[ ✔ ] All packages has been installed!"
+        echo "[ * ] Installing pip packages..."
         python3.10 -m pip install -r requirements.txt &>/dev/null && python3.10 -m pip install git+https://github.com/pytgcalls/pytgcalls -U &>/dev/null
-        success "[*] All pip packages has been installed!"
+        success "[ ✔ ] All pip packages has been installed!"
 
-        success "[*] Starting botnet..." && python3.10 main.py
-else 
-    script_exit_msg
-    exit 1;
-fi
-
-######################   WSL2   #####################################################
-
-if [[ "$OSTYPE" =~ ^WSL2 ]]; then
-    warning "[*] You are running script from WSL2, some botnet functions dont work on this platform"
-    echo "[*] Clonning botnet from git..."
+        success "[ ~ ] Starting botnet..." && python3.10 main.py
+elif [[ "$OSTYPE" =~ ^WSL2 ]]; then
+    warning "[ ! ] You are running script from WSL2, some botnet functions dont work on this platform"
+    echo "[ * ] Clonning botnet from git..."
     git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null
-    success "[*] Botnet clonned into your system!"
-    echo "[*] Entering to botnet directory..."
+    success "[ ✔ ] Botnet clonned into your system!"
+    echo "[ * ] Entering to botnet directory..."
     cd ~/telegram-raid-botnet
-    success "[*] All ok"
-    echo "[*] Installing packcages..."
-    sudo apt install -y python ffmpeg youtube-dl git &>/dev/null
-    success "[*] All packages has been installed!"
-    echo "[*] Installing pip packages..."
+    success "[ ✔ ] All ok"
+    echo "[ * ] Installing packcages..."
+    sudo apt install -y python ffmpeg youtube-dl git &>/dev/null || error "[ !! ] An error accurated, when script tryed to install packages"; warning "[ * ] Try to type sudo apt update && sudo apt upgrade, or install botnet from instruction"; exit 1;
+    success "[ ✔ ] All packages has been installed!"
+    echo "[ * ] Installing pip packages..."
     pip3 install -r requirements.txt  &>/dev/null
-    success "[*] All pip packages has been installed!"
+    success "[ ✔ ] All pip packages has been installed!"
 
-    success "[*] Starting botnet..." && python3 main.py
-else 
-    script_exit_msg
-    exit 1;
-fi
-
-#########################  ARCH LINUX BASED DISTROS  ##################################################
-
-if [ -f '/etc/arch-release' ]; then
-    ubuntu_python
-    echo "[*] Clonning botnet from git..."
+    success "[ ~ ] Starting botnet..." && python3 main.py
+elif cat /etc/*release | grep ^ID | grep arch &>/dev/null; then
+    echo "[ * ] Clonning botnet from git..."
     git clone https://github.com/json1c/telegram-raid-botnet.git ~/telegram-raid-botnet &>/dev/null
-    success "[*] Botnet clonned into your system!"
-    echo "[*] Entering to botnet directory..."
+    success "[ ✔ ] Botnet clonned into your system!"
+    echo "[ * ] Entering to botnet directory..."
     cd ~/telegram-raid-botnet
-    success "[*] All ok"
-    echo "[*] Installing packcages..."
-    sudo pacman -S ffmpeg git python-pip youtube-dl python -y &>/dev/null
-    success "[*] All packages has been installed!"
-    echo "[*] Installing pip packages..."
+    success "[ ✔ ] All ok"
+    echo "[ * ] Installing packcages..."
+    sudo pacman -S ffmpeg git python-pip youtube-dl python --noconfirm &>/dev/null || error "[ !! ] An error accurated, when script tryed to install packages"; warning "[ * ] Try to type pacman -Suyy, or install botnet from instruction"; exit 1;
+    success "[ ✔ ] All packages has been installed!"
+    echo "[ * ] Installing pip packages..."
     pip3 install -r requirements.txt  &>/dev/null && pip3 install git+https://github.com/pytgcalls/pytgcalls -U &>/dev/null
-    success "[*] All pip packages has been installed!"
+    success "[ ✔ ] All pip packages has been installed!"
 
-    success "[*] Starting botnet..." && python3 main.py  
+    success "[ ~ ] Starting botnet..." && python3 main.py  
 else 
     script_exit_msg
-    exit 1;
-fi
+    exit 0;
+    fi
